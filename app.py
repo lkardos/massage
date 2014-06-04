df21125e1f056fda01f06136390d19c69ecce3cf
Lubos Kardos <lkardos@redhat.com>
2014-06-04 16:46:20 +0200

    Added feature to offer massage to somebody.

--- app.py
+++ app.py
@@ -44,6 +44,59 @@ def get_data_timestamp():
     
     return jsonify(massages=massages, status="ok")
 
+@app.route("/offer")
+def offer():
+    massage_id = request.args.get("massage_id", None, type=int)
+    
+    try:
+        massage = Massage.query.get(massage_id)
+        if massage.name == '':
+            raise AttributeError('Nobody owns massage %d, it can\'t be offered' % massage_id);
+        massage.offered = True
+        db.session.commit()
+        
+    except (TypeError, AttributeError) as e:
+        return jsonify(status="error")
+    return jsonify(status="ok")
+
+@app.route("/take_form")
+def take_form():
+    massage_id = request.args.get("massage_id", None, type=int)
+    massage = Massage.query.get(massage_id)
+    if massage.name == '' or not massage.offered:
+        return render_template("sombody_was_quicker.html")
+    else: 
+        return render_template("take_form.html", massage_id=massage.id, massage_from=massage.start.isoformat(), massage_to=massage.end.isoformat())
+
+@app.route("/take_form_submit")
+def take_form_submit():
+    massage_id = request.args.get("massage_id", None, type=int)
+    user_name = request.args.get("user_name", None, type=str)
+    
+    massage = Massage.query.get(massage_id)
+    if massage.name == '' or not massage.offered:
+        return render_template("sombody_was_quicker.html")
+    massage.name = user_name
+    massage.offered = False
+    db.session.commit()
+    return render_template("succeed.html")
+        
+@app.route("/take")
+def take():
+    massage_id = request.args.get("massage_id", None, type=int)
+    user_name = request.args.get("user_name", None, type=str)
+    
+    try:
+        massage = Massage.query.get(massage_id)
+        if massage.name == '' or not massage.offered:
+            raise AttributeError('Sorry somebody was quicker than you (massage  id: %s)' % massage_id);
+        massage.name = user_name
+        massage.offered = False
+        db.session.commit()
+        
+    except (TypeError, AttributeError) as e:
+        return jsonify(status="error")
+    return jsonify(status="ok")
 
 @app.route("/set_data")
 def set_data():
--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class Massage(db.Model):
     start = db.Column(db.DateTime)
     end = db.Column(db.DateTime)
     name = db.Column(db.String(50))
+    offered = db.Column(db.Boolean())
 
     @property
     def serialize(self):
@@ -14,6 +15,7 @@ class Massage(db.Model):
             "start": self.start.isoformat(),
             "end": self.end.isoformat(),
             "name": self.name,
+            "offered": 1 if self.offered else 0,
         }
         
     @property
@@ -23,5 +25,6 @@ class Massage(db.Model):
             "start": int((self.start - datetime.datetime(1970, 1, 1)).total_seconds()),
             "end": int((self.end - datetime.datetime(1970, 1, 1)).total_seconds()),
             "name": self.name,
+            "offered": 1 if self.offered else 0,
         }
 
